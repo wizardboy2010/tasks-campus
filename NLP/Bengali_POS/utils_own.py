435e5ced442e9a5017be3bc05abd783b8162e8a4
Surya <tsuryateja.iitkgp@gmail.com>
2018-11-03 07:38:51 +0530

    updated utils

--- NLP/Bengali_POS/utils_own.py
+++ NLP/Bengali_POS/utils_own.py
@@ -2,12 +2,19 @@ import numpy as np
 
 def get_poslist(filename):
     with open(filename, 'r') as f:
-        pos = [l.strip().split(' ')[2] for l in f if(l.strip() != '' and l.strip().split(' ')[0] != '#')]
+        pos = [l.strip().split()[2] for l in f if(l.strip() != '' and l.strip().split()[0] != '#')]
     return set(pos)
 
-with open('Data/data1','r', encoding = 'utf-8') as f:
-	for l in f:
-		print(l.strip())
+# with open('Data/data1','r') as f:
+# 	for l in f:
+# 		try:
+# 			if '***' in l.strip().split()[2]:
+# 				print l.strip().split()
+# 		except:
+# 			pass
 
-#pos_list = get_poslist('Data/data1')
-#print(pos_list)
+pos_list = get_poslist('Data/data1')
+print pos_list, '\n'
+
+pos_list = get_poslist('Data/data2')
+print pos_list
